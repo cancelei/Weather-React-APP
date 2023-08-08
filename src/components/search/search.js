031a520c50485bd343b1db286d0ef5e72356652b
Glauber <glauber.bannwart@gmail.com>
2023-08-08 12:33:07 -0300

    Update APP.js & Search.js

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -7,7 +7,7 @@ const Search = ({ onSearchChange }) => {
 
   const loadOptions = (inputValue) => {
     return fetch(
-      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      `${GEO_API_URL}/cities?minPopulation=50000&namePrefix=${inputValue}`,
       geoApiOptions
     )
       .then((response) => response.json())
