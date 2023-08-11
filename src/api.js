9f2242b6e0a440473c0462178abae8b555bb84dc
Glauber <glauber.bannwart@gmail.com>
2023-08-10 23:05:30 -0300

    Add API Data (Hardcoded)

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 export const geoApiOptions = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    "X-RapidAPI-Key": "b14f62d621msh111f7947f28a8e7p1bfd9bjsn78a2abb36d89", // enter your rapid api key here
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    'X-RapidAPI-Key': 'b14f62d621msh111f7947f28a8e7p1bfd9bjsn78a2abb36d89', // enter your rapid api key here
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
   },
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "11f3c3f3c13a1d34b0bc14561cf7598e"; // enter your key from openweather API
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '11f3c3f3c13a1d34b0bc14561cf7598e'; // enter your key from openweather API
